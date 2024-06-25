4b7df3a31f9f040ee89860db40683bc69800b697
abinrajb <abin.r@Polussolutions.com>
2024-06-25 17:55:04 +0530

    deleted

--- portfolio/index.js
+++
@@ -1,8 +0,0 @@
-console.log(localStorage.getItem("flag"))
-if(localStorage.getItem("flag")=="success") {
-   
-}
-else{
-    alert("please login");
-    window.location.href = 'login.html'; // Redirect to the main page
-}
